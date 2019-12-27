433c6dbe12e95bc161e07e2d75936d579ae2f3e0
chengfei <chengfei@iuitech.com>
2019-12-27 15:21:53 +0800

    up

---
+++ index.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const ejs = require('ejs');
+const { promisify } = require('util')
+const { writeFile, readFile } = require('fs')
+const { join } = require('path')
+
+const fetchData = async (city = 'Chengdu,jinniu') => {
+  try {
+    const res = await axios({
+      url: `http://wttr.in/${city}`,
+      method: 'GET',
+      params: {
+        format: 'j1'
+      }
+    })
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
+
+const output = async () => {
+  try {
+    const data = await fetchData()
+    const file = await promisify(readFile)(join(__dirname, './result.ejs'), 'utf-8')
+    const ret = ejs.compile(file)({
+      currentCondition:data.current_condition
+    });
+    await promisify(writeFile)('result.html', ret)
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+output()
